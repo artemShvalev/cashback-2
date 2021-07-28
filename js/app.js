7b326b5d54bfc9f7add35561ae997483a322be4b
artemShvalev <60879559+artemShvalev@users.noreply.github.com>
2021-07-28 10:48:44 +0500

    init

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function calculateCashBack(specialCategoryPurchases, otherCategoryPurchases) {
+  const specialCategoryPercent = 0.03;
+  const otherCategoryPercent = 0.01;
+
+  const specialCategoryCashback = specialCategoryPurchases * specialCategoryPercent;
+  const otherCategoryCashback = otherCategoryPurchases * otherCategoryPercent;
+  const totalCashback = specialCategoryCashback + otherCategoryPercent;
+  const limit = 10000;
+
+  return {
+    specialCategoryCashback,
+    otherCategoryCashback,
+    totalCashback: totalCashback > limit ? limit : totalCashback
+  }
+}
+
+const cashback = calculateCashback(5000,10000,);
+console.log(cashback);
